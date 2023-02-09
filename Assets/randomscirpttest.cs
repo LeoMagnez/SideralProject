6546c358b70b7c70fd54e3bb4c51157abd4d3dd9
Tristan-hub-prog <76900319+Tristan-hub-prog@users.noreply.github.com>
2023-02-09 19:29:21 +0100

    Tristan 19:29

--- Assets/randomscirpttest.cs
+++ Assets/randomscirpttest.cs
@@ -7,34 +7,61 @@ public class randomscirpttest : MonoBehaviour
 {
     //brute method, a changé si possible
     [SerializeField]
-    private BoxCollider BoxCollider;
+    private GameObject GameObject;
     [SerializeField]
-    private BoxCollider BoxCollider1;
+    private GameObject GameObject1;
     [SerializeField]
-    private BoxCollider BoxCollider2;
+    private GameObject GameObject2;
     [SerializeField]
-    private BoxCollider BoxCollider3;
+    private GameObject GameObject3;
     [SerializeField]
-    private BoxCollider BoxCollider4;
+    private GameObject GameObject4;
     [SerializeField]
-    private BoxCollider BoxCollider5;
+    private GameObject GameObject5;
     [SerializeField]
-    private BoxCollider BoxCollider6;
+    private GameObject GameObject6;
     [SerializeField]
-    private BoxCollider BoxCollider7;
-
+    private GameObject GameObject7;
+    [SerializeField]
+    private GameObject GameObject8;
+    [SerializeField]
+    private GameObject GameObject9;
+    [SerializeField]
+    private GameObject GameObject10;
+    private void Start()
+    {
+        GameObject.SetActive(true);
+        GameObject1.SetActive(false);
+        GameObject2.SetActive(false);
+        GameObject3.SetActive(false);
+        GameObject4.SetActive(false);
+        GameObject5.SetActive(false);
+        GameObject6.SetActive(false);
+        GameObject7.SetActive(false);
+        GameObject8.SetActive(false);
+        GameObject9.SetActive(false);
+        GameObject10.SetActive(false);
+    }
     public void PRINTEVENT(String S)
     {
         Debug.Log(S);
-        BoxCollider.enabled = true;
-        BoxCollider1.enabled = true;
-        BoxCollider2.enabled = true;
-        BoxCollider3.enabled = true;
-        BoxCollider4.enabled = true;
-        BoxCollider5.enabled = true;
-        BoxCollider6.enabled = true;
-        BoxCollider7.enabled = true;
+        GameObject.SetActive(false);
+        GameObject1.SetActive(true);
+        GameObject2.SetActive(true);
+        GameObject3.SetActive(true);
+        GameObject4.SetActive(true);
+        GameObject5.SetActive(true);
+        GameObject6.SetActive(true);
+        GameObject7.SetActive(true);
+        GameObject8.SetActive(true);
+        GameObject9.SetActive(true);
+        GameObject10.SetActive(true);
+    }
 
-        gameObject.SetActive(false);
+    public void PRINTEVENT2(String D)
+    {
+        Debug.Log(D);
+
+        this.gameObject.SetActive(false);
     }
 }
